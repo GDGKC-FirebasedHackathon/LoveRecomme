14bc41550149b1a2e8424c416e3bb91417d34143
Sangsu <racersyun@naver.com>
2017-02-17 17:05:08 +0900

    Activity Added

---
+++ app/src/main/java/com/dlinkddns/racersyun/loverecomme/RecommendCourseActivity.java
@@ -0,0 +1,13 @@
+package com.dlinkddns.racersyun.loverecomme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecommendCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend_course);
+    }
+}
