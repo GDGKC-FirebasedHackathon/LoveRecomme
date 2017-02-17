7b45c262089a4b2a10fff1071626eed5af2a2c8d
GIYOUNGBYUN <rain8496@naver.com>
2017-02-18 03:53:53 +0900

    추천메뉴

--- app/src/main/java/com/dlinkddns/racersyun/loverecomme/RecommendCourseActivity.java
+++ app/src/main/java/com/dlinkddns/racersyun/loverecomme/RecommendCourseActivity.java
@@ -1,12 +1,25 @@
 package com.dlinkddns.racersyun.loverecomme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class RecommendCourseActivity extends AppCompatActivity {
+    ListView listView1;
+    IconTextListAdapter adapter;
+    Button alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +31,43 @@ public class RecommendCourseActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        listView1 = (ListView)findViewById(R.id.listView1);
+
+        // 어댑터 객체 생성
+        adapter = new IconTextListAdapter(this);
+
+        // 아이템 데이터 만들기
+        Resources res = getResources();
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "강남토끼정", "30,000 다운로드", "900 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon06), "하남돼지집", "26,000 다운로드", "1500 원"));
+        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.icon05), "쉑쉑버거", "300,000 다운로드", "900 원"));
+
+
+        // 리스트뷰에 어댑터 설정
+        listView1.setAdapter(adapter);
+
+        // 새로 정의한 리스너로 객체를 만들어 설정
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                IconTextItem curItem = (IconTextItem) adapter.getItem(position);
+                String[] curData = curItem.getData();
+
+                Toast.makeText(getApplicationContext(), "Selected : " + curData[0], Toast.LENGTH_LONG).show();
+
+            }
+
+        });
+        alert = (Button)findViewById(R.id.alert);
+        alert.setOnClickListener((View.OnClickListener) this);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -28,6 +76,7 @@ public class RecommendCourseActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+
     }
 
     @Override
@@ -36,5 +85,16 @@ public class RecommendCourseActivity extends AppCompatActivity {
         mIntent.putExtra("keyExtra", "결과내용");
         setResult(RESULT_OK, mIntent);
         finish();
+    }
+    public void onClick(View view){
+        if(view == alert){
+            new AlertDialog.Builder(this).setTitle("목록").setMessage("Fine")
+                    .setNeutralButton("닫기", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dlg, int sumthin){
+
+                        }
+                    })
+                    .show();
+        }
     }
 }
