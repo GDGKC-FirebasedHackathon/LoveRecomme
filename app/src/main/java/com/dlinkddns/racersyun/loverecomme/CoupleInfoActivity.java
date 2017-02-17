e8bf31830397281b96b4e2193df80b4b5e953028
Sangsu <racersyun@naver.com>
2017-02-17 19:41:43 +0900

    중간점검

--- app/src/main/java/com/dlinkddns/racersyun/loverecomme/CoupleInfoActivity.java
+++ app/src/main/java/com/dlinkddns/racersyun/loverecomme/CoupleInfoActivity.java
@@ -1,13 +1,40 @@
 package com.dlinkddns.racersyun.loverecomme;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
+import static android.provider.CalendarContract.CalendarCache.URI;
 
 public class CoupleInfoActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_couple_info);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.inflateMenu(R.menu.activity_couple_info);
     }
 }
